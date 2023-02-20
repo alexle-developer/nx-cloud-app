2a227a5ddebb613d3d64257a78ac55f3ad5dd251
alexle-developer <alex.le.developer@gmail.com>
2023-02-19 21:36:40 -0600

    add ngrx diagram & clean up product service

--- libs/products/src/lib/services/products.service.ts
+++ libs/products/src/lib/services/products.service.ts
@@ -1,6 +1,6 @@
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
-import {BehaviorSubject, Observable, of, tap} from 'rxjs';
+import {BehaviorSubject, Observable} from 'rxjs';
 import {Product} from '../product.model';
 
 @Injectable({
